e1dc49f7d7932514fa8d0d117de6ef30e76adeef
sean <sean.gongz@gmail.com>
2020-10-05 23:55:15 +0200

    Dash layout and style

--- app.py
+++ app.py
@@ -13,19 +13,27 @@ df = pd.DataFrame({
     "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
 })
 
+colors = {'background': '#111111', 'text': '#7FDBFF'}
+
 fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+fig.update_layout(plot_bgcolor=colors['background'],
+                  paper_bgcolor=colors['background'],
+                  font_color=colors['text'])
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
+    html.H1(children='Hello Dash',
+            style={
+                'textAlign': 'center',
+                'color': colors['text']
+            }),
     html.Div(children='''
         Dash: A web application framework for Python.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
+    ''',
+             style={
+                 'textAlign': 'center',
+                 'color': colors['text']
+             }),
+    dcc.Graph(id='example-graph-2', figure=fig)
 ])
 if __name__ == '__main__':
     app.run_server(debug=True)
